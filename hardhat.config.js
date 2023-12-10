b92574321bf0e81d0d366c035393c08e0802a0da
Baranaba Mugabane <bashybaranaba@Baranabas-MacBook-Pro.local>
2023-12-10 12:34:53 +0300

    hardhat config update

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,7 @@ module.exports = {
     hardhat: {
       chainId: 1337,
     },
+    //Celo
     alfajores: {
       chainId: 44787,
       url: `https://celo-alfajores.infura.io/v3/${projectId}`,
@@ -15,6 +16,28 @@ module.exports = {
     celo: {
       chainId: 42220,
       url: "https://forno.celo.org",
+      accounts: [privateKey],
+    },
+    //Base
+    basemainnet: {
+      url: "https://mainnet.base.org",
+      accounts: [privateKey],
+      gasPrice: 1000000000,
+    },
+    basegoerli: {
+      url: "https://goerli.base.org",
+      accounts: [privateKey.toString()],
+      gasPrice: 1000000000,
+    },
+    baselocal: {
+      url: "http://localhost:8545",
+      accounts: [privateKey],
+      gasPrice: 1000000000,
+    },
+    //Filecoin
+    calibration: {
+      chainId: 314159,
+      url: "https://api.calibration.node.glif.io/rpc/v1",
       accounts: [privateKey],
     },
   },
